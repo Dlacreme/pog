2e2bb2f88f8f95011c53c5fc6cfd50c360a2a0d1
Alex-min <contact@alex-min.fr>
2021-02-21 13:44:51 +0100

    missing roleid

--- lib/pog/accounts/user.ex
+++ lib/pog/accounts/user.ex
@@ -32,7 +32,7 @@ defmodule Pog.Accounts.User do
   """
   def registration_changeset(user, attrs, opts \\ []) do
     user
-    |> cast(attrs, [:email, :password])
+    |> cast(attrs, [:email, :password, :role_id])
     |> validate_email()
     |> validate_password(opts)
   end
