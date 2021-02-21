edc164f53ab575ed3f2851b10eabaf472659b93e
Alex-min <contact@alex-min.fr>
2021-02-21 14:41:09 +0100

    adding

--- lib/pog/schema.ex
+++ lib/pog/schema.ex
@@ -5,6 +5,14 @@ defmodule Pog.Schema do
       @primary_key {:id, :binary_id, autogenerate: true}
       @foreign_key_type :binary_id
       @derive {Phoenix.Param, key: :id}
+
+      def first do
+        __MODULE__ |> Ecto.Query.first() |> Distanciel.Repo.one()
+      end
+
+      def last do
+        __MODULE__ |> Ecto.Query.last() |> Distanciel.Repo.one()
+      end
     end
   end
 end
